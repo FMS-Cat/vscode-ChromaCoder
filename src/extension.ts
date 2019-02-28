8c9ae0e0dcf0b39675442bf32a297bcb57e92890
FMS-Cat <yutaka.obuchi@gmail.com>
2019-03-01 04:29:05 +0900

    h

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,32 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+function applyDecoration( editor: vscode.TextEditor ) {
+  const textDecoration = vscode.window.createTextEditorDecorationType( <vscode.DecorationRenderOptions>{
+    textDecoration: `none; background: rgba(0, 0, 0, 1);`,
+    rangeBehavior: vscode.DecorationRangeBehavior.ClosedClosed,
+  } );
+  editor.setDecorations( textDecoration, [ new vscode.Range(
+    new vscode.Position ( 0, 0 ),
+    new vscode.Position( editor.document.lineCount, 9999 )
+  ) ] );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "chromacoder" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+  return textDecoration;
 }
 
-// this method is called when your extension is deactivated
+export function activate( context: vscode.ExtensionContext ) {
+  console.log( 'Congratulations, your extension "chromacoder" is now active!' );
+
+  context.subscriptions.push( ...[
+    vscode.commands.registerCommand( 'chromacoder.activate', () => {
+      const activeEditor = vscode.window.activeTextEditor;
+      if ( !activeEditor ) { return; }
+
+      context.subscriptions.push( applyDecoration( activeEditor ) );
+    } ),
+    vscode.commands.registerCommand( 'chromacoder.deactivate', () => {
+      console.error( 'TODO' );
+    } )
+  ] );
+}
+
 export function deactivate() {}
