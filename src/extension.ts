298bc1a7585849f19b4af84282e3d1b91d017031
FMS-Cat <yutaka.obuchi@gmail.com>
2019-03-01 23:25:23 +0900

    It now can preserve activation status to another editor

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,8 @@
 import * as vscode from 'vscode';
 
+let isActivated = false;
+let currentDecoration: vscode.TextEditorDecorationType | null = null;
+
 function applyDecoration( editor: vscode.TextEditor ) {
   const textDecoration = vscode.window.createTextEditorDecorationType( <vscode.DecorationRenderOptions>{
     textDecoration: `none; background: rgba(0, 0, 0, 1);`,
@@ -10,22 +13,35 @@ function applyDecoration( editor: vscode.TextEditor ) {
     new vscode.Position( editor.document.lineCount, 9999 )
   ) ] );
 
+  currentDecoration = textDecoration;
   return textDecoration;
 }
 
 export function activate( context: vscode.ExtensionContext ) {
-  console.log( 'Congratulations, your extension "chromacoder" is now active!' );
-
   context.subscriptions.push( ...[
     vscode.commands.registerCommand( 'chromacoder.activate', () => {
       const activeEditor = vscode.window.activeTextEditor;
       if ( !activeEditor ) { return; }
 
+      isActivated = true;
       context.subscriptions.push( applyDecoration( activeEditor ) );
     } ),
+
     vscode.commands.registerCommand( 'chromacoder.deactivate', () => {
-      console.error( 'TODO' );
-    } )
+      isActivated = false;
+
+      if ( currentDecoration ) {
+        currentDecoration.dispose();
+      }
+    } ),
+
+    vscode.window.onDidChangeActiveTextEditor( ( editor ) => {
+      if ( !isActivated ) { return; }
+
+      if ( editor ) {
+        context.subscriptions.push( applyDecoration( editor ) );
+      }
+    }, null )
   ] );
 }
 
